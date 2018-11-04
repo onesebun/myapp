430c5b668e13907d57eeeceb97803190bc83127d
17 <lyc1029@kfsyscc.org>
2018-11-04 20:29:02 +0800

    navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold (
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[      // Add 3 lines from here...
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -93,6 +96,38 @@ class RandomWordsState extends State<RandomWords> {
       },
     );
   }
+
+  void _pushSaved() {
+  Navigator.of(context).push(
+    new MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+        return new Scaffold(         // Add 6 lines from here...
+          appBar: new AppBar(
+            title: const Text('Saved Suggestions'),
+          ),
+          body: new ListView(children: divided),
+        );                           // ... to here.
+      },
+    ),
+  );
+}
 }
 
 class RandomWords extends StatefulWidget {
