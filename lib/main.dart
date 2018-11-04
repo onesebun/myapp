8bf817d0fe3d45c8a121029ef68d2d2d922cc8c0
17 <lyc1029@kfsyscc.org>
2018-11-04 20:31:50 +0800

    change the UI using Themes

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
       //   ),
       // ),
       title: 'Startup Name Generator',
+      theme: new ThemeData(          // Add the 3 lines from here... 
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
